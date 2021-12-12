b28ef9f0da4d0e39cafe2dece8475f59d9613cbe
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-12 14:17:48 +0600

    commit

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -37,7 +37,7 @@ const facebookProvider = new FacebookAuthProvider();
  
 
   useEffect(() => {
-    fetch(`http://localhost:5000/users/${user.email}`)
+    fetch(`https://shielded-eyrie-93201.herokuapp.com/users/${user.email}`)
         .then(res => res.json())
         .then(data => setAdmin(data.admin))
   }, [user.email]);
@@ -79,7 +79,7 @@ useEffect(() =>{
 
 const saveUser = (email, displayName, method) =>{
   const user = {email, displayName};
-  fetch('http://localhost:5000/users', {
+  fetch('https://shielded-eyrie-93201.herokuapp.com/users', {
     method: method,
     headers: {
       'content-type' : 'application/json'
