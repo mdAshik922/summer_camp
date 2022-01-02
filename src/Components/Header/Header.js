6644b80401cf275b06e588b5a49107b0f9d8b31b
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-02 12:46:57 +0600

    commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
 
-    <Nav className="me-auto " style={{marginLeft: "10%"}}>
+    <Nav className="me-auto" style={{marginLeft: "10%"}}>
       <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
       <Nav.Link as={NavLink} to="/about">About</Nav.Link>
       <Nav.Link as={NavLink} to="/team">Team</Nav.Link>
